d13abec88f83031d6df45a79bffaada9561cd9d2
73nko <apramos89@gmail.com>
2020-04-30 15:21:30 +0200

    My first graph! :tada:

--- 01-making-your-first-chart/draft/chart.js
+++ 01-making-your-first-chart/draft/chart.js
@@ -1,6 +1,66 @@
-async function drawLineChart() {
-  // write your code here
+import dimensions from "./dimensions.js";
 
+const dateParser = d3.timeParse("%Y-%m-%d");
+const yAccessor = ({ temperatureMax }) => temperatureMax;
+const xAccessor = ({ date }) => dateParser(date);
+const yScale = d3.scaleLinear();
+const xScale = d3.scaleTime();
+const lineGenerator = d3.line();
+
+async function drawLineChart() {
+  // Fetching data
+  const dataset = await d3.json("../../my_weather_data.json");
+
+  // We create the wrapper element and give it dimensions
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  // we create a bound element and give it the dimensions
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px`
+    );
+
+  // Creating the x and y scales
+  yScale
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0]);
+
+  xScale
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth]);
+
+  // A rect tos show the freefinz area
+  const freezingTemperaturePlacement = yScale(32);
+  const freefinzTemperatures = bounds
+    .append("rect")
+    .attr("x", 0)
+    .attr("width", dimensions.boundedWidth)
+    .attr("y", freezingTemperaturePlacement)
+    .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+    .attr("fill", "#e0f3f3");
+
+  // creating our line
+  lineGenerator.x((d) => xScale(xAccessor(d))).y((d) => yScale(yAccessor(d)));
+  const line = bounds.append("path").attr("d", lineGenerator(dataset));
+
+  // Drawinng the axes
+  const yAxisGenerator = d3.axisLeft().scale(yScale);
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const yAxis = bounds.append("g").call(yAxisGenerator);
+  const xAxis = bounds
+    .append("g")
+    .call(xAxisGenerator)
+    // we need to position the x axis at the bottom manualy
+    .style("transform", `translatey(${dimensions.boundedHeight}px)`);
+
+  // style our line
+  line.attr("fill", "none").attr("stroke", "#af9358").attr("stroke-width", 2);
 }
 
-drawLineChart()
+drawLineChart();
---
+++ 01-making-your-first-chart/draft/dimensions.js
@@ -0,0 +1,17 @@
+let dimensions = {
+  width: window.innerWidth * 0.9,
+  height: 400,
+  margin: {
+    top: 15,
+    right: 15,
+    bottom: 40,
+    left: 60,
+  },
+};
+
+dimensions.boundedWidth =
+  dimensions.width - dimensions.margin.left - dimensions.margin.right;
+dimensions.boundedHeight =
+  dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+export default dimensions;
