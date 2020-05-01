8b23846d9da855bfac8359aaca5f54ed62930c39
73nko <apramos89@gmail.com>
2020-05-01 18:59:16 +0200

    Second chart and starting the third

--- 02-making-a-scatterplot/draft/draw-scatter.js
+++ 02-making-a-scatterplot/draft/draw-scatter.js
@@ -1,5 +1,99 @@
 async function drawScatter() {
-  // your code goes here
-  
+  // 1. Access data
+  const dataset = await d3.json("../../my_weather_data.json");
+  const xAccessor = ({ dewPoint }) => dewPoint;
+  const yAccessor = ({ humidity }) => humidity;
+
+  //// Bonus. Adding another scale, color
+  const colorAccessor = ({ cloudCover }) => cloudCover;
+
+  // 2. Set dimensions
+  const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
+  const dimensions = {
+    width: width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // 3. Draw the canvas
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
+
+  // 4. Create Scales
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+    .nice();
+
+  //// Bonus. Creating color scale
+  const colorScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, colorAccessor))
+    .range(["skyBlue", "darkslategrey"]);
+
+  // 5. Draw data
+  const dots = bounds
+    .selectAll("circle")
+    .data(dataset)
+    .join("circle")
+    .attr("cx", (d) => xScale(xAccessor(d)))
+    .attr("cy", (d) => yScale(yAccessor(d)))
+    .attr("r", 5)
+    .attr("fill", (d) => colorScale(colorAccessor(d)));
+
+  // 6. Draw Peripherals
+  //// X axis
+  const xAxisGenerator = d3.axisBottom().scale(xScale);
+  const xAxis = bounds
+    .append("g")
+    .call(xAxisGenerator)
+    .style("transform", `translateY(${dimensions.boundedHeight}px)`);
+  const xAxisLabel = xAxis
+    .append("text")
+    .attr("x", dimensions.boundedWidth / 2)
+    .attr("y", dimensions.margin.bottom / 2)
+    .attr("fill", "darkgrey")
+    .style("font-size", "1.4em")
+    .html("Dew point (&deg;F)");
+
+  //// Y axis
+  const yAxisGenerator = d3.axisLeft().scale(yScale).ticks(4);
+  const yAxis = bounds.append("g").call(yAxisGenerator);
+  const yAxisLabel = yAxis
+    .append("text")
+    .attr("x", -dimensions.boundedHeight / 2)
+    .attr("y", -dimensions.margin.left + 10)
+    .attr("fill", "darkgrey")
+    .style("font-size", "1.4em")
+    .style("transform", "rotate(-90deg)")
+    .style("text-anchor", "middle")
+    .html("Relative humidity");
 }
-drawScatter()
+drawScatter();
--- 03-making-a-bar-chart/draft/draw-bars.js
+++ 03-making-a-bar-chart/draft/draw-bars.js
@@ -1,5 +1,39 @@
 async function drawBars() {
-  // your code goes here
+  // 1. Access data
+  const dataset = await d3.json("../../my_weather_data.json");
+  const metricAccessor = ({ humidity }) => humidity;
 
+  // 2. Set dimensions
+  const width = 600;
+  const dimensions = {
+    width: width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+
+  dimensions.boundedHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  // 3. Draw the canvas
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 }
-drawBars()
+drawBars();
